f9c232ad77de63461d7f9e0063db6dc15128b79d
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2025-02-07 20:17:12 +0100

    feat(main): Use TileMapLayers

--- game.gd
+++ game.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
-func assign_texture(tile : Node) -> void:
-	tile.find_child("ground").texture = get_meta("tile_types")[tile.get_meta("type")]
-
 func _ready() -> void:
-	var tile_node = preload("res://Tile.tscn")
-	var tiles : Array = get_meta("tiles")
+	var map : TileMapLayer = find_child("Map")
 	for x in range(0, 5):
 		for y in range(0, 5):
-			var tile : Node = tile_node.instantiate()
-			tile.set_meta("pos", Vector2i(x, y))
-			tile.set_meta("type", 5)
-			assign_texture(tile)
-			add_child(tile)
-			tiles.insert(0, tile)
+			pass
+
+func is_neighbour(a : Vector2i, b : Vector2i) -> bool:
+	return (((a.x - 1 <= b.x and b.x <= a.x + 1)
+			and (b.y == a.y or b.y == a.y - 1))
+		or (b.y == a.y + 1 and b.x == a.x))
+
+func _input(event: InputEvent) -> void:
+	var map : TileMapLayer = find_child("Map")
+	var player : Node2D = find_child("Player")
+	if (event is InputEventMouseButton and event.pressed
+		and event.button_index == MOUSE_BUTTON_LEFT):
+			var target_pos : Vector2i = map.local_to_map(to_local(event.position))
+			#if is_neighbour(target_pos, player.get_meta("pos")):
+				#player.set_meta("pos", target_pos)
 
 func _process(_delta: float) -> void:
 	pass
