54031e60c58db28dc930295070c5d7904ddb643a
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2025-02-07 17:16:19 +0100

    feat(main): Add example map generation

--- game.gd
+++ game.gd
@@ -4,7 +4,16 @@ func assign_texture(tile : Node) -> void:
 	tile.find_child("ground").texture = get_meta("tile_types")[tile.get_meta("type")]
 
 func _ready() -> void:
-	pass
+	var tile_node = preload("res://Tile.tscn")
+	var tiles : Array = get_meta("tiles")
+	for x in range(0, 5):
+		for y in range(0, 5):
+			var tile : Node = tile_node.instantiate()
+			tile.set_meta("pos", Vector2i(x, y))
+			tile.set_meta("type", 5)
+			assign_texture(tile)
+			add_child(tile)
+			tiles.insert(0, tile)
 
 func _process(_delta: float) -> void:
 	pass
