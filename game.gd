8182aa080939ac31858b292c111447591854e09f
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2025-02-08 13:22:37 +0100

    feat(main): Add conditional overlays

--- game.gd
+++ game.gd
@@ -26,6 +26,12 @@ func is_neighbour(a : Vector2i, b : Vector2i) -> bool:
 			and (b.y == a.y or b.y == a.y - 1))
 		or (b.y == a.y + 1 and b.x == a.x))
 
+func is_impassable(pos : Vector2i) -> bool:
+	return false
+
+func has_action(pos : Vector2i) -> bool:
+	return false
+
 func move_player(pos : Vector2i) -> void:
 	var ground : TileMapLayer = find_child("Ground Layer")
 	var entities : TileMapLayer = find_child("Entity Layer")
@@ -33,7 +39,8 @@ func move_player(pos : Vector2i) -> void:
 	var target_pos : Vector2i = ground.local_to_map(to_local(pos))
 	var player_pos : Vector2i = get_meta("player_pos")
 	if (is_neighbour(target_pos, player_pos)
-		and ground.get_cell_source_id(target_pos) != -1):
+		and target_pos in ground.get_used_cells()
+		and !is_impassable(target_pos)):
 		ui.set_cell(player_pos,
 			ui.get_cell_source_id(player_pos),
 			Vector2i(0, ui.get_cell_atlas_coords(player_pos).y))
@@ -43,14 +50,20 @@ func move_player(pos : Vector2i) -> void:
 
 func move_mouse_highlighting(offset : Vector2) -> void:
 	var ui : TileMapLayer = find_child("UI Layer")
+	var player_pos : Vector2i = get_meta("player_pos")
 	var new_pos : Vector2i = ui.local_to_map(get_local_mouse_position())
 	var old_pos : Vector2i = ui.local_to_map(get_local_mouse_position() - offset)
 	ui.set_cell(old_pos,
 		-1 if ui.get_cell_atlas_coords(old_pos).x == 0 else 0,
 		Vector2i(ui.get_cell_atlas_coords(old_pos).x, 0))
 	if (new_pos in find_child("Ground Layer").get_used_cells()):
+		var overlay_type : int = (4 if (new_pos != player_pos
+			and new_pos in find_child("Entity Layer").get_used_cells())
+			else (1 if (!is_neighbour(new_pos, player_pos)
+				or (new_pos == player_pos and !has_action(new_pos)))
+				else (3 if is_impassable(new_pos) else 2)))
 		ui.set_cell(new_pos, 0, Vector2i((ui.get_cell_atlas_coords(new_pos).x
-			if ui.get_cell_atlas_coords(new_pos).x >= 0 else 0), 1))
+			if ui.get_cell_atlas_coords(new_pos).x >= 0 else 0), overlay_type))
 
 func _input(event: InputEvent) -> void:
 	if (event is InputEventMouseButton and event.pressed
