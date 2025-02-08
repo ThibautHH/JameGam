ebb357da88902aa52d4cd65afb19ade5990472b0
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2025-02-09 00:31:01 +0100

    feat(main): Add tile generation and defeat

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,22 +8,18 @@ var player_pos : Vector2i
 var player_stamina : int = 1
 var player_max_stamina : int = 1
 
+var lost : bool = false
+
 const all_directions : Array = [
+	TileSet.CellNeighbor.CELL_NEIGHBOR_TOP_LEFT_SIDE,
 	TileSet.CellNeighbor.CELL_NEIGHBOR_TOP_SIDE,
 	TileSet.CellNeighbor.CELL_NEIGHBOR_TOP_RIGHT_SIDE,
 	TileSet.CellNeighbor.CELL_NEIGHBOR_BOTTOM_RIGHT_SIDE,
 	TileSet.CellNeighbor.CELL_NEIGHBOR_BOTTOM_SIDE,
-	TileSet.CellNeighbor.CELL_NEIGHBOR_BOTTOM_LEFT_SIDE,
-	TileSet.CellNeighbor.CELL_NEIGHBOR_TOP_LEFT_SIDE]
+	TileSet.CellNeighbor.CELL_NEIGHBOR_BOTTOM_LEFT_SIDE]
 
 func get_random_tile() -> Vector2i:
 	return Vector2i(randi_range(0, 4), randi_range(0, 4))
-
-func get_random_empty_neighbour(pos : Vector2i) -> Vector2i:
-	return (all_directions.map(func(direction):
-			return ground_layer.get_neighbor_cell(pos, direction))
-		.filter(func(pos): return !ground_layer.get_used_cells().has(pos))
-		.pick_random())
 
 func make_starting_tiles() -> void:
 	var center : Vector2i = ground_layer.local_to_map(get_viewport_rect().size / 2)
@@ -42,12 +38,16 @@ func make_starting_tiles() -> void:
 func _ready() -> void:
 	make_starting_tiles()
 
+func is_void(pos : Vector2i) -> bool:
+	return !(pos in ground_layer.get_used_cells())
+
+func get_neighbours(pos : Vector2i) -> Array:
+	return all_directions.map(func(direction):
+		return ground_layer.get_neighbor_cell(pos, direction))
+
 func is_neighbour(a : Vector2i, b : Vector2i) -> bool:
 	return all_directions.any(func(direction):
 		return a == ground_layer.get_neighbor_cell(b, direction))
-
-func is_impassable(pos : Vector2i) -> bool:
-	return ground_layer.get_cell_tile_data(pos).get_custom_data("impassable")
 
 func get_tile_movement_cost(pos : Vector2i) -> int:
 	return (ground_layer.get_cell_tile_data(pos)
@@ -61,20 +61,41 @@ func move_player(pos : Vector2i) -> void:
 	player_pos = pos
 	player_stamina -= get_tile_movement_cost(pos)
 
+func is_impassable(pos : Vector2i) -> bool:
+	return ground_layer.get_cell_tile_data(pos).get_custom_data("impassable")
+
 func can_move_to(pos : Vector2i) -> bool:
 	return !is_impassable(pos) and player_stamina >= 1
 
 func is_tile_possible_destination(pos : Vector2i) -> bool:
-	return (is_neighbour(pos, player_pos)
-		and pos in ground_layer.get_used_cells())
+	return (is_neighbour(pos, player_pos) and !is_void(pos))
+
+func is_surrounded(pos : Vector2i) -> bool:
+	return !(get_neighbours(pos).any(is_void))
+
+func get_random_empty_neighbour(pos : Vector2i) -> Vector2i:
+	return (get_neighbours(pos).filter(is_void).pick_random())
+
+func end_turn() -> void:
+	ground_layer.set_cell(get_random_empty_neighbour(player_pos),
+		0, get_random_tile())
+	if is_surrounded(player_pos):
+		lost = true
+		return
+	if player_stamina < player_max_stamina:
+		player_stamina += 1
 
 func interact_with_tile(pos : Vector2i):
-	if pos == player_pos:
-		pass
-	if (is_tile_possible_destination(pos) and can_move_to(pos)):
-		move_player(pos)
+	if pos != player_pos:
+		if (is_tile_possible_destination(pos) and can_move_to(pos)):
+			move_player(pos)
+			end_turn()
+	else:
+		end_turn()
 
 func _input(event: InputEvent) -> void:
+	if lost:
+		return
 	if (event is InputEventMouseButton and event.pressed
 		and event.button_index == MOUSE_BUTTON_LEFT):
 			interact_with_tile(
